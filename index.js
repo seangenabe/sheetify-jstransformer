c691f2490d4c23c4302274fd3568041ccba6e754
Arve Knudsen <arve.knudsen@gmail.com>
2017-11-26 19:49:06 +0100

    Ignore a plugin if it doesn't match the input file type

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var identity = function(a) { return a }
 module.exports = function sheetify_jstransform(filename, source, options, cb) {
   var use = [].concat(options.use)
   var current = source
+
   function runTransform() {
     try {
       var useSingle = use.shift()
@@ -12,7 +13,15 @@ module.exports = function sheetify_jstransform(filename, source, options, cb) {
       }
       useSingle = [].concat(useSingle)
       var transformer = jstransformer(useSingle[0])
-      var opts = useSingle[1]
+      var matchesTransformer = transformer.inputFormats.some(function (format) {
+        return RegExp('\.' + format + '$').test(filename)
+      })
+      if (!matchesTransformer) {
+        runTransform()
+        return
+      }
+
+      var opts = useSingle[1] || {}
       var optsCb = useSingle[2] || identity
       opts = optsCb(opts, filename)
       transformer.renderAsync(current, opts, {}, function(err, result) {
